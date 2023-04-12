e16e526da2b1d60ed7cfd476c7d7c1d928964ab8
milcennii <jamilhusni.abdulhanif@b2be.com>
2023-04-12 13:13:24 +0800

    modify realtime checking

--- app/Http/Livewire/Posts.php
+++ app/Http/Livewire/Posts.php
@@ -3,16 +3,19 @@
 namespace App\Http\Livewire;
 use Livewire\Component;
 use App\Models\counter;
+use App\Models\sejarah;
 
 class Posts extends Component
 {
-    public $posts, $title, $body, $post_id;
+    public $posts, $title, $body, $post_id, $history;
     public $counter = 0;
     public $testd = '';
     public $arr = [];
+    public $limit;
     public function render()
     {
         $this->counter = counter::select('count')->first()->count;
+        $this->history = sejarah::latest()->limit(5)->get();
         return view('livewire.posts');
     }
 
@@ -30,17 +33,17 @@ class Posts extends Component
 
         // $ccs = 0;
         // return 'arr length :' . count($this->arr);
-        for($i = 0; $i < count($this->arr); $i++)
-        {
-            $ch = curl_init('http://intranet.b2be.com/bcs/uploads/avatar/' . $this->arr[$i] . '');
-            $fp = fopen(storage_path('app/downloaded/' . $this->arr[$i] . ''), 'wb');
-            curl_setopt($ch, CURLOPT_FILE, $fp);
-            curl_setopt($ch, CURLOPT_HEADER, 0);
-            curl_exec($ch);
-            curl_close($ch);
-            fclose($fp);
-            // $ccs++;
-        }
+        // for($i = 0; $i < count($this->arr); $i++)
+        // {
+        //     $ch = curl_init('http://intranet.b2be.com/bcs/uploads/avatar/' . $this->arr[$i] . '');
+        //     $fp = fopen(storage_path('app/downloaded/' . $this->arr[$i] . ''), 'wb');
+        //     curl_setopt($ch, CURLOPT_FILE, $fp);
+        //     curl_setopt($ch, CURLOPT_HEADER, 0);
+        //     curl_exec($ch);
+        //     curl_close($ch);
+        //     fclose($fp);
+        //     // $ccs++;
+        // }
         // return $ccs;
     }
 
@@ -56,6 +59,62 @@ class Posts extends Component
         $updateCount->count = 0;
         $updateCount->save();
 
+        // $file = fopen(storage_path('app/downloaded/sqlquery.txt'), 'r');
+        // $counss = 0;
+        // if ($file) {
+        //     while (($line = fgets($file)) !== false) {
+        //         // process each line here
+        //         if($counss != 0)
+        //         {
+        //             // return $line;
+        //             $pattern = '/uploads\/avatar\/(.+?)\'/';
+        //             preg_match($pattern, $line, $matches);
+        //             // return $matches[1];
+        //             if (isset($matches[1])) {
+        //                 $this->testd = $matches[1];
+        //                 array_push($this->arr, $matches[1]);
+        //             }
+        //         }
+        //         $counss++;
+        //     }
+
+        //     fclose($file);
+        // }
+    }
+    
+    public function pollingChanges()
+    {
+        // $this->counter = counter::find(1);
+        $this->counter = counter::select('count')->first()->count;
+        $this->history = sejarah::latest()->limit(5)->get();
+    }
+
+    public function setLimit()
+    {
+        $this->fetchImageData();
+        $sej = new sejarah;
+        $sej->bil = $this->limit;
+        $sej->semasa = now();
+        $sej->save();
+    }
+
+    public function fetchImageData()
+    {
+        for($i = 0; ($i < count($this->arr)) && ($i < $this->limit); $i++)
+        {
+            $ch = curl_init('http://intranet.b2be.com/bcs/uploads/avatar/' . $this->arr[$i] . '');
+            $fp = fopen(storage_path('app/downloaded/' . $this->arr[$i] . ''), 'wb');
+            curl_setopt($ch, CURLOPT_FILE, $fp);
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_exec($ch);
+            curl_close($ch);
+            fclose($fp);
+            // $ccs++;
+        }
+    }
+
+    public function getArrayOfImage()
+    {
         $file = fopen(storage_path('app/downloaded/sqlquery.txt'), 'r');
         $counss = 0;
         if ($file) {
--- resources/views/livewire/posts.blade.php
+++ resources/views/livewire/posts.blade.php
@@ -1,8 +1,36 @@
 <div> 
-    <h1 style="margin-top : 150px;">
+    <h1 style="margin-top : 150px;" wire:poll='pollingChanges'>
         {{ $counter }} <br>
-        {{ $testd }}
     </h1>
     <button wire:click="tambahCounter" type="button" class="btn btn-dark">Add(+)</button>
     <button wire:click="clearCounter" type="button" class="btn btn-dark">Clear(-)</button>
+    <hr>
+
+    <form wire:submit.prevent='setLimit'>
+        <div class="row">
+            <div class="col-7">
+                <h1>Number of image retrieved : {{ count($this->arr) }}</h1>
+            </div>
+            <div class="col">
+                <button style="display : block; margin-top : 8px;" wire:click="getArrayOfImage" type="button" class="btn btn-success">Fetch Image List</button>
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="exampleInputLimit">Limit the image</label>
+            {{-- <input type="text" class="form-control" wire:model="posts" aria-describedby="LimitHelp" placeholder="Enter Limit"> --}}
+            <input type="text" class="form-control" wire:model.defer="limit" aria-describedby="LimitHelp" placeholder="Enter Limit">
+            <small id="LimitHelp" class="form-text text-muted">Put the number of image you want to retrieved</small>
+        </div>
+        <button type="submit" class="btn btn-primary">Retrieve Image</button>
+        <small id="LimitHelp" class="form-text text-muted">Downloaded locaton : live-data\storage\app\downloaded</small>
+    </form>
+    <br>
+    <h1>History</h1>
+    <div class="container">
+        @foreach($history as $hist)
+            <div class="alert alert-success" role="alert">
+                ({{ $hist->bil }}) images has been retrieved on {{ $hist->semasa }}
+            </div>
+        @endforeach
+    </div>
 </div>
